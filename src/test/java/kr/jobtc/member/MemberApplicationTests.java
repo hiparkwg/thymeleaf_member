0e06b7f281c1807962f4468cd55d1a30942cb75e
hiparkwg <hiparkwg@gmail.com>
2024-05-14 21:59:33 +0900

    init

---
+++ src/test/java/kr/jobtc/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jobtc.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
